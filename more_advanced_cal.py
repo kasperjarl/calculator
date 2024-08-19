13a55cb6104a159daa4c179081cf428a1e2c79db
root <root@FIB-bCsriQNY2YA.fibia.local>
2024-08-19 19:29:53 +0200

    dunno if this is the correct way to manipulate the string

--- more_advanced_cal.py
+++ more_advanced_cal.py
@@ -4,30 +4,32 @@
 
 class Calculator:
     def __init__(self):
-        print("Hello I'm the advanced calculator.\nYou can use these operations: +, -, *, /. \nInput the entire math problem in one go ")
-        self.math_problem = input()            
+        print("Hello I'm the bit more advanced calculator.\nYou can use these operations: +, -, *, /. \nInput the entire math problem in one go WITH SPACES!")
+        self.math_problem = str(input())            
         self.allowed_ops = ['+','-','*','/']
-        self.nums = ['1','2','3','4','5','6','7','8','9']
-        self.numbers = []
+        # self.nums = ['1','2','3','4','5','6','7','8','9']
         self.ops = []
         print(f"Let's solve the problem: {self.math_problem}")
-        print(self.problem_cleaning)
+        self.problem_cleaning()
+        # self.do_math()
         
     def problem_cleaning(self):
         # what if we make use of two stacks: one for numbers and one for operations
         # --> What do we do when the number is more than one diget? Then we can't use for char in string.
         # --> What do we do with 12.241? The "dot"
-        ops = []
-        numbers = []
+        # --> What do we do with: 2 * (3+4)?
+       
+        
+        self.prob = self.math_problem.split()
+        self.res = []
+        self.res.append(self.prob[0])
 
-        for char in self.math_problem:
-            print(char)
-            if char in self.allowed_ops:
-                ops.append(char)
-            elif char in self.nums:
-                numbers.append(char)
+        # now we need to some math with it..
+
+    def do_math(self):
+        pass
 
     def __repr__(self) -> str:
-        return f"ops: {self.ops}\nnumbers: {self.numbers}"
+        return f"self.prob: {self.prob}\nself.ops: {self.res}"
 
 print(Calculator())
