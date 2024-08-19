90eeedeb8e081edcb743e4775e97c20b23da6447
root <root@FIB-bCsriQNY2YA.fibia.local>
2024-08-19 08:45:16 +0200

    updated

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-print("Hello there! \nI'm Cal the calculator. \nWhat's your name?\n")
-name = input()
-print(f"Hello {name}. \nWhat do you want to calculate?\n type '+' for addition\n type '-' for subtraction\n type '*' for multiplication\n type '/' for divison")
+print(f"Hello! I'm Cal the calculator. \nWhat do you want to calculate?\n\n type '+' for addition\n type '-' for subtraction\n type '*' for multiplication\n type '/' for divison\n type 'quit' to quit the calculator")
 
-type = input("pick one: ")
-number_one = 1
-number_two = 2
+def calculator():
+    allowedOps = ['+', '-', '*', '/', 'quit']
+    
+    type = input("\npick one: ")
+    if type not in allowedOps:
+        return "You need to pick one of these: '+', '-', '*', '/', 'quit' "
 
-def calculator(type, number_one: int, number_two: int):
-    allowd = ['+', '-', '*', '/']
-    if type not in allowd:
-        return "You need to pick one of these: '+', '-', '*', '/' "
+    number_one: int = float(input("\nenter the first number: "))
+
+    number_two = float(input("\nenter the second number: "))
+
+    if type == 'quit':
+        return "bye!"
 
     elif type == '+':
         return number_one + number_two
     
-print(calculator(type, number_one, number_two))
+    elif type == '-':
+        return number_one - number_two
+    
+    elif type == '*':
+        return number_one * number_two
+    
+    elif type == '/':
+        return number_one / number_two
+    
+    else:
+        return "something went wrong"
+    
+print(calculator())
