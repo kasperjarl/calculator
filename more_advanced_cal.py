8917c2ef6c0bfedec7f69065dec75f13844c9ed4
root <root@FIB-bCsriQNY2YA.fibia.local>
2024-08-20 07:25:04 +0200

    more_advanced_cal is now working. But we need to figure out how to handle 2 * 2 + (1 + 2)

--- more_advanced_cal.py
+++ more_advanced_cal.py
@@ -7,29 +7,47 @@ class Calculator:
         print("Hello I'm the bit more advanced calculator.\nYou can use these operations: +, -, *, /. \nInput the entire math problem in one go WITH SPACES!")
         self.math_problem = str(input())            
         self.allowed_ops = ['+','-','*','/']
-        # self.nums = ['1','2','3','4','5','6','7','8','9']
-        self.ops = []
+        self.res = 0
         print(f"Let's solve the problem: {self.math_problem}")
         self.problem_cleaning()
-        # self.do_math()
+        self.do_math()
         
     def problem_cleaning(self):
-        # what if we make use of two stacks: one for numbers and one for operations
-        # --> What do we do when the number is more than one diget? Then we can't use for char in string.
-        # --> What do we do with 12.241? The "dot"
-        # --> What do we do with: 2 * (3+4)?
-       
-        
-        self.prob = self.math_problem.split()
-        self.res = []
-        self.res.append(self.prob[0])
+        self.operations = self.math_problem.split()
+        self.res = float(self.operations[0])
+        self.operations = self.operations[1:]
 
-        # now we need to some math with it..
 
     def do_math(self):
-        pass
+        while self.operations:
+            if self.operations[0] == '+':
+                self.addition()
+            elif self.operations[0] == '-':
+                self.subtraction()
+            elif self.operations[0] == '*':
+                self.multiplication()
+            elif self.operations[0] == '/':
+                self.division()
+            else:
+                raise ValueError("You can only use these operations: +, -, *, /, no () and there must be spaces in between.")
+
+    def addition(self):
+        self.res += float(self.operations[1])
+        self.operations = self.operations[2:]
+
+    def subtraction(self):
+        self.res -= float(self.operations[1])
+        self.operations = self.operations[2:]
+
+    def multiplication(self):
+        self.res *= float(self.operations[1])
+        self.operations = self.operations[2:]
+    
+    def division(self):
+        self.res /= float(self.operations[1])
+        self.operations = self.operations[2:]
 
     def __repr__(self) -> str:
-        return f"self.prob: {self.prob}\nself.ops: {self.res}"
+        return f"self.operations: {self.operations}\nself.res: {self.res}"
 
 print(Calculator())
